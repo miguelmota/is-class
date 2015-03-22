50886cba55974d0385487c74ea752c5cb15c518b
Miguel Mota <miguelmota2@gmail.com>
2015-03-21 21:10:02 -0700

    init

---
+++ is-class.js
@@ -0,0 +1,21 @@
+(function(root) {
+
+  function isClass(fn) {
+    return typeof fn === 'function' && /^class\s/.test(Function.prototype.toString.call(fn));
+  }
+
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = isClass;
+    }
+    exports.isClass = isClass;
+  } else if (typeof define === 'function' && define.amd) {
+    define([], function() {
+      return isClass;
+    });
+  } else {
+    root.isClass = isClass;
+  }
+
+})(this);
+

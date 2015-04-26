f9aed2f1803f95875ffb3e6820bd5a7802b0dc28
Miguel Mota <miguelmota2@gmail.com>
2015-04-26 14:45:15 -0700

    babel.js classes

--- is-class.js
+++ is-class.js
@@ -1,7 +1,15 @@
 (function(root) {
+  var toString = Function.prototype.toString;
+
+  function fnBody(fn) {
+    return toString.call(fn).replace(/^[^{]*{\s*/,'').replace(/\s*}[^}]*$/,'');
+  }
 
   function isClass(fn) {
-    return typeof fn === 'function' && /^class\s/.test(Function.prototype.toString.call(fn));
+    return (typeof fn === 'function' &&
+            (/^class\s/.test(toString.call(fn)) ||
+              (/^_classCallCheck\(/.test(fnBody(fn)))) // babel.js
+            );
   }
 
   if (typeof exports !== 'undefined') {
